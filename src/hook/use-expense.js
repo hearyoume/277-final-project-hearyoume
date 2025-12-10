1ae43b0e2d17edbc6481540c53a57f64996a2dd2
Amanda Ellis <amanda.ellis002@student.swic.edu>
2025-12-10 15:35:03 -0600

    Add category management functionality to useExpense hook

--- src/hook/use-expense.js
+++ src/hook/use-expense.js
@@ -1,8 +1,26 @@
 import { useLocalStorage } from "@uidotdev/usehooks";
 import { useState } from "react";
 
+const DEFAULT_CATEGORIES = [
+  "Groceries",
+  "Eating Out",
+  "Vacation",
+  "Entertainment",
+  "Utilities",
+  "Toiletries",
+  "Home Upkeep",
+  "Mortgage",
+  "Health",
+  "Amazon",
+  "Other",
+];
+
 export default function useExpense() {
   const [expenses, setExpenses] = useLocalStorage("expenses", []);
+  const [categories, setCategories] = useLocalStorage(
+    "categories",
+    DEFAULT_CATEGORIES
+  );
   const [filter, setFilter] = useState("all");
   const [amount, setAmount] = useState("");
   const [place, setPlace] = useState("");
@@ -10,6 +28,8 @@ export default function useExpense() {
   const [recurring, setRecurring] = useState("one-time");
   const [touchedAmount, setTouchedAmount] = useState(false);
   const [touchedPlace, setTouchedPlace] = useState(false);
+  const [showNewCategory, setShowNewCategory] = useState(false);
+  const [newCategoryName, setNewCategoryName] = useState("");
 
   const addExpense = (expense) => {
     setExpenses((prevExpenses) => [...prevExpenses, expense]);
@@ -17,6 +37,18 @@ export default function useExpense() {
 
   const deleteExpense = (id) => {
     setExpenses((prevExpenses) => prevExpenses.filter((e) => e.id !== id));
+  };
+
+  const addCategory = (newCategory) => {
+    if (newCategory && !categories.includes(newCategory)) {
+      setCategories((prevCategories) => [...prevCategories, newCategory]);
+    }
+  };
+
+  const deleteCategory = (categoryToDelete) => {
+    setCategories((prevCategories) =>
+      prevCategories.filter((cat) => cat !== categoryToDelete)
+    );
   };
 
   return {
@@ -37,5 +69,12 @@ export default function useExpense() {
     touchedPlace,
     setTouchedPlace,
     deleteExpense,
+    categories,
+    addCategory,
+    deleteCategory,
+    showNewCategory,
+    setShowNewCategory,
+    newCategoryName,
+    setNewCategoryName,
   };
 }
