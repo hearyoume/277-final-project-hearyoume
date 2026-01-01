fbe2375e53d522338da219e69b76070522f679b5
Amanda Ellis <amanda.ellis002@student.swic.edu>
2026-01-01 16:20:09 -0600

    Update Delete button styling to add padding and give width consistent to other buttons

--- src/components/expenses/expense-item.jsx
+++ src/components/expenses/expense-item.jsx
@@ -8,7 +8,7 @@ export default function ExpenseItem({ expense, handleDeleteExpense }) {
       {/* Delete button */}
       <button
         onClick={() => handleDeleteExpense(expense.id)}
-        className="ml-4 px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600"
+        className="ml-4 min-w-[100px] px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600"
       >
         Delete
       </button>
